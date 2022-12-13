b804909669df7f93627c32afd00bcb966037ddaa
Juan Aviles <avilesjuan628@gmail.com>
2022-12-13 14:46:38 -0500

    updated version of gym app, implemented api

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Box, Stack, Typography, Button} from '@mui/material'
+import HeroBannerImage from '../assets/images/banner.png'
+
+function HeroBanner() {
+  return (
+    <Box sx={{
+        mt: { lg: '212px', xs: '70px'}, 
+        ml: { sm: '50px'}
+    }} position='relative' p='20px'>
+        <Typography color='red' 
+        fontWeight='600' fontSize='24px'>
+            Fitness Club
+        </Typography>
+        <Typography fontWeight='700' sx={{ 
+            fontSize: { lg: '44px', xs: '40px'} 
+        }} mb='23px' mt='30px'>
+            Sweet, Smile <br /> and Repeat
+        </Typography>
+        <Typography fontSize='22px' lineHeight='35px' mb={4}>
+            Check out some of the best exercises. 
+        </Typography>
+        <Button variant='contained' color='error' href='#exercises' sx={{ backgroundColor:'red', padding: '10px'}}>Explore Exercises</Button>
+        <Typography 
+            fontWeight={600}
+            color='red'
+            sx={{ 
+                opacity: 0.1, 
+                display: {lg: 'block', xs: 'none'}
+            }} fontSize='200px'
+            >
+                Exercise
+            </Typography>
+        
+        <img src={HeroBannerImage} alt='hero-img' className='hero-banner-img' />
+    </Box>
+  )
+}
+
+export default HeroBanner
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
     <Stack 
         direction='row'
         justifyContent='space-around'
-        sx={{gap: { sm: '122px', xs: '40px', mt: {sm: '32px', xs:'20px'}}}}>
+        sx={{gap: { sm: '122px', xs: '40px', mt: {sm: '32px', xs:'20px'}}, justifyContent: 'none'}} px='20px'>
       <Link to='/'>
         <img src={newLogo} style={{ height: '48px', width: '48px', margin: '0 20px'}} />
       </Link>
@@ -19,7 +19,7 @@ const Navbar = () => {
         fontSize='24px' 
         alignItems='flex-end'
         >
-      <Link to='/'  style={{textDecoration: 'none', color: '#3a1212', borderBottom: '3px solid gray'}}>Home</Link>
+      <Link to='/'  style={{textDecoration: 'none', color: '#3a1212', borderBottom: '3px solid red'}}>Home</Link>
       <a href='#exercises' style={{ textDecoration: 'none', color: '#3A1212'}}>Exercises</a>
     </Stack>    
     </Stack>
